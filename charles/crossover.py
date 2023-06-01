a510149e499214db20ef3c453ae50eac8f653ab6
Felix Gaber <107850501+felitsch@users.noreply.github.com>
2023-06-01 09:47:54 +0100

    Update crossover.py

--- charles/crossover.py
+++ charles/crossover.py
@@ -11,7 +11,7 @@ def geometric_xo(p1,p2):
         r = np.random.uniform(size=len(p1[layer]))
         for elem in range(0,len(p1[layer])):
             offspring1[layer][elem] = r[elem]*p1[layer][elem] + (1-r[elem])*p2[layer][elem]
-            offspring2[layer][elem] = r[elem]*p1[layer][elem] + (1-r[elem])*p2[layer][elem]
+            offspring2[layer][elem] = r[elem]*p2[layer][elem] + (1-r[elem])*p1[layer][elem]
     return offspring1, offspring2
 
 
@@ -22,7 +22,7 @@ def arithmetic_xo(p1,p2):
     for layer in range(len(p1)):
         for elem in range(0, len(p1[layer])):
             offspring1[layer][elem] = p1[layer][elem]*alpha + (1-alpha)*p2[layer][elem]
-            offspring2[layer][elem] = p1[layer][elem]*alpha + (1-alpha)*p2[layer][elem]
+            offspring2[layer][elem] = p2[layer][elem]*alpha + (1-alpha)*p1[layer][elem]
     return offspring1, offspring2
 
 
